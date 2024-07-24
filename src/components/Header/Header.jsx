e49da92e0c50ccec0147fc19c5f50116beaf5341
Jace-H <jacehowell.d@gmail.com>
2024-07-23 21:08:50 -0600

    Configured tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
     <>
-    <h1 className="header">Avian Sanctuary and Protection</h1>
+    <h1 className="header text-red-500">Avian Sanctuary and Protection</h1>
     <button className="nav-menu-btn">Menu</button>
     </>
     )
