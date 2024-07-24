c1c9bf87a2fdb1a3530e49b25e91991bd731785b
Jace-H <jacehowell.d@gmail.com>
2024-07-23 20:46:41 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Header from './components/Header/Header.jsx'
+
+const App = () => {
+  return (
+  <Header/>
+)
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+    <>
+    <h1 className="header">Avian Sanctuary and Protection</h1>
+    <button className="nav-menu-btn">Menu</button>
+    </>
+    )
+}
