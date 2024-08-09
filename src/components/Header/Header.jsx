3ba5e24ddb35774c0c3c98b7bef17c4ce68244b1
Jace-H <jacehowell.d@gmail.com>
2024-08-08 21:28:44 -0600

    Added mobile nav menu functionality and About page content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,54 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-    <header className="header bg-blue-green text-white flex justify-between top-0 w-full p-2.5">
-        <Link to='/' className="header-text p-2.5">Avian Sanctuary and Protection</Link>
-        <button className="nav-menu-btn md:hidden text-blue-300 relative right-2.5">Menu</button>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="header bg-blue-green text-white flex justify-between top-0 w-full p-2.5">
+        <Link to="/" className="header-text p-2.5">
+          Avian Sanctuary and Protection
+        </Link>
+
+        <button
+          onClick={toggleMenu}
+          className="nav-menu-btn md:hidden text-blue-300 relative right-2.5"
+        >
+          Menu
+        </button>
+
+
         <ul className="nav-menu-bar hidden md:flex">
-            <Link to='/' className="text-gray-200 p-2.5">Home</Link>
-            <Link to='/services' className="text-gray-200 p-2.5">Services</Link>
-            <Link to='/about' className="text-gray-200 p-2.5">About</Link>
+          <Link to="/" className="text-gray-200 p-2.5">
+            Home
+          </Link>
+          <Link to="/services" className="text-gray-200 p-2.5">
+            Services
+          </Link>
+          <Link to="/about" className="text-gray-200 p-2.5">
+            About
+          </Link>
         </ul>
-    </header>
-    )
+      </header>
+      {isMenuOpen && (
+        <nav className="flex bg-slate-300 justify-center pb-2 md:hidden">
+          <ul>
+          <Link to="/" className="p-2.5">
+            Home
+          </Link>
+          <Link to="/services" className="p-2.5">
+            Services
+          </Link>
+          <Link to="/about" className="p-2.5">
+            About
+          </Link>
+          </ul>
+        </nav>
+      )}
+    </>
+  );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,15 +4,38 @@ import Footer from "../components/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
 const About = () => {
-    return (
-      <>
-        <Header />
-        <p>About Test</p>
-        <Footer />
-        <Outlet/>
-      </>
-    );
-  };
-  
-  export default About;
-  
+  return (
+    <>
+      <Header />
+      <h2 className="flex justify-center p-4 pt-6 text-4xl">About</h2>
+
+      <div>
+        <img></img>
+        <p className="text-lg p-2 italic">
+          We believe in raising avian awareness and helping people understand
+          how to coexist with their natural environment. We wish our birds could
+          speak to you themselves about their plight in the wild; of struggling
+          to exist alongside cities with glass office buildings in their
+          migratory paths, of running into cars unexpectedly, of losing their
+          hunting lands to subdivisions, of being misunderstood and shot at, and
+          of extreme weather conditions. However, since they can't, we have made
+          it our ambition to do just that.
+        </p>
+      </div>
+
+      <div>
+        <img></img>
+        <p className="text-lg p-2 italic">
+          The Avian Sanctuary and Protection Agency is a Utah State and Federal
+          non-profit 501(c) 3 organization that will focus on the two pronged
+          mission of rehabilitation of local birds as well as educational
+          programs within the community.
+        </p>
+      </div>
+      <Footer />
+      <Outlet />
+    </>
+  );
+};
+
+export default About;
